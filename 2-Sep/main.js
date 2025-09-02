0ff69a8ef6a43cf9f620cd47580d25fb05bb73d1
itx-mj65 <jamshedweb65@gmail.com>
2025-09-02 15:02:52 +0500

    2 of sep

---
+++ 2-Sep/main.js
@@ -0,0 +1,17 @@
+document.writeln("Hello, World!");
+alert("hello world")
+console.log("hello world")
+
+
+"hello world 68 639%^%&"  // string
+74658   // number
+985368356386593869589469n  // BigInt
+true or false  // boolean
+null     // null 
+let a;   // undefined
+[468, "helo", 63484 , false , null ]   // array 
+
+{
+    num : 468,
+
+}
