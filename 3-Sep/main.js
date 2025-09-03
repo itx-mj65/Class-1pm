83ccc79330b39b47caff37e2c2a2bfb609d79a0b
itx-mj65 <jamshedweb65@gmail.com>
2025-09-03 14:36:06 +0500

    3 of sep

---
+++ 3-Sep/main.js
@@ -0,0 +1,31 @@
+// var class_dev = "hello world"    /// declaration
+
+// class_dev = "hello universe"     // overriding
+
+// var class_dev = "hello everyone"   /// redeclaration
+
+
+
+
+// let a = "hello this is let " 
+
+// a= "hello this is let again"   /// overriding
+
+// let a = "hello this is let again and again"  /// redeclaration error
+
+// const b = "hello this is const"   /// declaration
+
+// b="hello this is const again"   /// overriding
+
+// const b = "hello this is const again and again"   /// redeclaration error
+
+
+
+// console.log(b)
+// console.log(class_dev)    /// logging
+
+{
+    let blok = {};
+
+};
+console.log(blok)  /// block scope
